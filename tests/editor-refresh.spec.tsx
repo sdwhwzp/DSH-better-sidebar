ea70cfd087b0a85cd35a33b80d7f5b746f1e6893
sdwhwzp <sdwhwzp@126.com>
2026-09-02 15:42:11 +0800

    feat: add universal file downloads

--- tests/editor-refresh.spec.tsx
+++ tests/editor-refresh.spec.tsx
@@ -21,6 +21,7 @@ import { allLeaves, createSidebarStore, type SidebarTab } from '../src/client/st
 const fsRead = vi.fn()
 vi.mock('../src/client/api.ts', () => ({
   api: { fsRead: (...args: unknown[]) => fsRead(...args) },
+  downloadUrl: () => '/sidebar/file',
   mediaUrl: () => '',
 }))
 
